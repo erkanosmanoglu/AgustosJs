56667e68e0cfdbcb982286b59bc999be3232e338
Erkan Osmanoglu <erkanosmanoglu1626@gmail.com>
2026-04-05 20:38:18 +0300

    gun30

---
+++ Gun30/script.js
@@ -0,0 +1,24 @@
+const img = document.getElementById("picture");
+const canvas = document.getElementById("canvas");
+
+// gösterilecek resim dosyalarının adları
+let imgs = [
+    "animal.jpg", 
+    "city.jpg", 
+    "flower.jpg", 
+    "lake.jpg", 
+    "mountain.jpg"
+];
+
+// gösterilen resmin indexi
+
+let i = 0;
+
+function goster(degisim = 0) {
+    i+= degisim;
+    img.remove(); //animasyon için
+    img.src = "img/" + imgs.at(i % 5);
+    canvas.appendChild(img); //animasyon için
+}
+
+goster();
